6e7689711a4b04d20655378fb7e483434dd96dfc
Wahyu Prihantoro <wahyuprihantoro21@gmail.com>
2017-11-01 07:05:00 +0700

    add test stg 1

--- sol.py
+++ sol.py
@@ -97,7 +97,7 @@ def train_neural_network(x):
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
     optimizer = tf.train.AdamOptimizer().minimize(cost)
 
-    hm_epochs = 10
+    hm_epochs = 2
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
